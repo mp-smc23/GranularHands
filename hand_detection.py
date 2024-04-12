47463af56a54eb430ff693c3dc4d7835390af624
Maria Polak <mpolak23@student.aau.dk>
2024-04-12 14:32:56 +0200

    Gesture Detection

--- hand_detection.py
+++ hand_detection.py
@@ -1,84 +1,92 @@
-
 import mediapipe as mp
+import cv2
+import numpy as np
+
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
-import numpy as np
-import cv2
-import time
-import matplotlib.pyplot as plt
 
 MARGIN = 10  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
-base_options = python.BaseOptions(model_asset_path='model/hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
+class HandDetection:
+  def __init__(self) -> None:
+      self.cap = cv2.VideoCapture(0)
+      self.cap.set(cv2.CAP_PROP_FPS, 5)
+      self.detector = self.init_detector()
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
+      self.starting_position = None
 
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
+  def __del__(self) -> str:
+     self.cap.release()
+     pass
 
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
+  def init_detector(self):
+    base_options =  python.BaseOptions(model_asset_path='model/gesture_recognizer.task')
+    options = vision.GestureRecognizerOptions(base_options=base_options, num_hands=2)
+    return vision.GestureRecognizer.create_from_options(options)
 
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
+  def run(self):
+    while True: 
+        ret, frame = self.cap.read() 
 
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
-
-
-def view():
-    cap = cv2.VideoCapture(0) # init camera
-    
-    while True: # video "callback"
-        ret, frame = cap.read() # read the frame
-        cap.set(cv2.CAP_PROP_POS_FRAMES,10)
-
-        if not ret: # if frame can't be read or user clicked the "Stop" button
-            cap.release()
+        if not ret: 
+            self.cap.release()
+            cv2.destroyAllWindows()
+            return
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-        detection_result = detector.detect(mp_image)
-        annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
-        
+        detection_result = self.detector.recognize(mp_image)
+        annotated_image = self.draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
+
         cv2.imshow("Video", cv2.flip(annotated_image, 1) )
 
-        # cv2.imshow('Video', annotated_image)
-        #time.sleep(1)
         if cv2.waitKey(1) & 0xFF == ord('q'): 
-          break
-    
-    cap.release()
+          return
 
+  @staticmethod
+  def is_resetting_position(detection_result):
+    # Check if the hand is in the starting position which is two fists
+    hand_landmarks_list = detection_result.hand_landmarks
+    if len(hand_landmarks_list) != 2:
+      return False
 
-view()
+    # Check if the landmarks for both hands are close to each other
+    hand_landmarks_1 = hand_landmarks_list[0]
+    hand_landmarks_2 = hand_landmarks_list[1]
+    distance_threshold = 0.1  # Adjust this threshold as needed
+    for landmark_1, landmark_2 in zip(hand_landmarks_1, hand_landmarks_2):
+      distance = np.sqrt((landmark_1.x - landmark_2.x)**2 + (landmark_1.y - landmark_2.y)**2)
+      if distance > distance_threshold:
+        return False
+
+    return True
+  @staticmethod
+  def calculate_hand_positions(detection_result):
+     pass
+
+  @staticmethod
+  def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+      hand_landmarks = hand_landmarks_list[idx]
+
+      # Draw the hand landmarks.
+      hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+      hand_landmarks_proto.landmark.extend([
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+      ])
+      mp.solutions.drawing_utils.draw_landmarks(
+        annotated_image,
+        hand_landmarks_proto,
+        mp.solutions.hands.HAND_CONNECTIONS,
+        mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
+        mp.solutions.drawing_styles.get_default_hand_connections_style())
+
+    return annotated_image
---
+++ main.py
@@ -0,0 +1,7 @@
+import hand_detection as hd
+
+# init the hand detection class
+hand_detector = hd.HandDetection()
+hand_detector.run()
+
+
